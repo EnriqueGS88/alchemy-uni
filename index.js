270aeed24ef014adf208821b568176338b4889ab
EnriqueGS88 <enrique.gzs@gmail.com>
2023-01-05 00:27:22 +0100

    fixed color hashing and search

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 // the possible colors that the hash could represent
 const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
+const sampleHash = 'c685a2c9bab235ccdd2ab0ea92281a521c8aaf37895493d080070ea00fc7f5d7';
 
 // given a hash, return the color that created the hash
-function findColor(  ) {
+function findColor( hash ) {
+    const colorHash = COLORS.map(element => toHex( sha256(utf8ToBytes(element))));
+    const indexOfColor = colorHash.indexOf( hash );
 
-    const colorHash = COLORS.forEach(element => sha256(utf8ToBytes(element)));
-
-    // const indexOfColor = colorHash.find(  hashes );
-
-
-
-    console.log( colorHash );
-
-    
+    return COLORS[ indexOfColor ];
 }
 
-findColor(  );
+findColor( sampleHash );
