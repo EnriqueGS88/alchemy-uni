d0f0f6c75964aec40a9e38fd3c3b640ad14b0228
EnriqueGS88 <enrique.gzs@gmail.com>
2023-01-05 22:37:31 +0100

    passed tests of signMessage script

--- week1/signMessage.js
+++ week1/signMessage.js
@@ -4,7 +4,23 @@ const hashMessage = require('./hashMessage');
 const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
 
 async function signMessage(msg) {
+
+    const hashMsg = hashMessage( msg );
+
+    // recovered: true - sets the function to return the "recovery bit"
+    // recovery bit is used to check who signed this message
+    const signedMessage = await secp.sign( 
+        hashMsg, 
+        PRIVATE_KEY,
+        {
+            recovered: true,
+        } 
+        );
+    console.log( signedMessage );
+
+    return signedMessage;
     
 }
 
 module.exports = signMessage;
+// signMessage( 'hello world' );
