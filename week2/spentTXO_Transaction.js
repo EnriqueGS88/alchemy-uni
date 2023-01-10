dc51071e302f90b334103a730567b67be2e6427d
EnriqueGS88 <enrique.gzs@gmail.com>
2023-01-11 00:59:13 +0100

    solved tests for checking if an input TXO has been spent

--- week2/spentTXO_Transaction.js
+++ week2/spentTXO_Transaction.js
@@ -5,11 +5,16 @@ class Transaction {
         this.outputUTXOs = outputUTXOs;
         
     }
+    
     execute() {
 
-        if ( this.inputUTXOs != this.outputUTXOs ) {
+        // Access the .spent property stored in each TXO instance
+        // then store the value of .spent into a new Array "spentFlag"
+        const spentFlag = this.inputUTXOs.map( element => element.spent );
 
-
+        // if there is one single "true" stored in the new spentFlag array
+        // throw an error
+        if ( spentFlag.includes( true ) == true ) {
             throw new Error( "tx has already been spent" );
         }
         
