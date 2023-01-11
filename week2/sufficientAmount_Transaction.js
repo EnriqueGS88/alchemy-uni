f89a9193bcfbaa25eb35decc2067913d8f9d1e86
EnriqueGS88 <enrique.gzs@gmail.com>
2023-01-11 01:30:33 +0100

    passed tests on sufficient amount

---
+++ week2/sufficientAmount_Transaction.js
@@ -0,0 +1,40 @@
+const TXO = require('../week2/sufficientAmount_TXO');
+
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+
+        this.inputUTXOs = inputUTXOs;
+        this.outputUTXOs = outputUTXOs;
+        
+    }
+    
+    execute() {
+
+        // Access the .spent property stored in each TXO instance
+        // then store the value of .spent into a new Array "spentFlag"
+        const spentFlag = this.inputUTXOs.map( element => element.spent );
+        
+        // if there is one single "true" stored in the new spentFlag array
+        // throw an error
+        if ( spentFlag.includes( true ) == true ) {
+            throw new Error( "tx has already been spent" );
+        }
+        
+        
+        const inputAmountsArray = this.inputUTXOs.map( element => element.amount );
+        const outputAmountsArray = this.outputUTXOs.map( element => element.amount );
+
+        // Use reduce to get the Summatory of an Array
+
+        const inputAmounts =  inputAmountsArray.reduce( ( acc, curr ) => acc + curr );
+        const outputAmounts =  outputAmountsArray.reduce( ( acc, curr ) => acc + curr );
+        const amountsComparison = inputAmounts - outputAmounts;
+
+        if ( amountsComparison !== 0 ) {
+            throw new Error( "total output amounts differ from total input amounts" );
+        }
+        
+    }
+}
+
+module.exports = Transaction;
